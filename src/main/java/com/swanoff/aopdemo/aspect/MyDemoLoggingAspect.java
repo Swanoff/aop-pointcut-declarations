b9c5af7a2e91e0b939bf70cd6694e476a8778268
Surya <suryathegreat.07@gmail.com>
2020-04-14 13:26:03 +0530

    Reusing Pointcut declaration for other advices

--- src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,10 +8,6 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-
-	// this is where we add all of our related advices for logging
-	
-	// lets start with an @Before advice
 	
 	@Pointcut("execution(* com.swanoff.aopdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
@@ -19,5 +15,10 @@ public class MyDemoLoggingAspect {
 	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====> Executing @Before advice on method");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====> Performing API analytics");
 	}
 }
